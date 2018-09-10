e0f9171cac2b4be6224ca979b29de4492e44a379
TAREQ HASAN <akmhasan18@gmail.com>
2018-09-09 20:09:45 -0400

    Another commit

--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -5,13 +5,23 @@ package math.problems;
  */
 public class FindMissingNumber {
 
+    static int getMissingNum (int array[], int x)
+    {
+        int i, sum;
+        sum  = (x+1)*(x+2)/2;
+        for ( i = 0; i< x; i++)
+            sum -= array[i];
+        return sum;
+    }
+
     public static void main(String[] args) {
         /*
          * If n = 10, then array will have 9 elements in the range from 1 to 10.
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
-
+         int [] array = new int[]{10, 2, 1, 7, 5, 3, 8, 9, 6};
+        int missNum = getMissingNum(array, 9);
+        System.out.println(missNum);
     }
 }
--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Scanner;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,45 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+    String pnt1, pnt2;
+    int sel1, sel2, sel3, i, j;
+    int Anagram = 0, notAnagram = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter the first String: ");
+        pnt1 = scan.nextLine();
+        System.out.println("Please enter the Second String: ");
+        pnt2 = scan.nextLine();
+
+        sel2 = pnt1.length();
+        sel3 = pnt2.length();
+
+        if(sel2 == sel3){
+            sel1 = sel2;
+            for (i = 0; i < sel1; i++){
+                Anagram = 0;
+                for (j = 0; j<sel1; j++ ){
+                    if(pnt1.charAt(i) == pnt2.charAt(j)){
+                        Anagram = 1;
+                        break;
+                    }
+                }
+
+                if (Anagram == 0){
+                    notAnagram = 1;
+                    break;
+                }
+            }
+            if (notAnagram == 1){
+                System.out.println("These are not Anagram Strings");
+            }
+            else {
+                System.out.println("These are Anagram Strings");
+            }
+        }
+        else {
+            System.out.println("Both Strings has to be the same number of character to be the Anagram");
+        }
+
     }
 }
