c3c84f81b8411b0fea55fc7b42512895b1d2fd44
TAREQ HASAN <akmhasan18@gmail.com>
2018-09-10 16:51:59 -0400

    added array list

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +12,32 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		List<String> list = new ArrayList<>();
+		System.out.println("Size of Array list at Creation: " +list.size());
 
+		//Adding some elements
+		list.add("Tareq");
+		list.add("Siddique");
+		list.add("Shams");
+		list.add("Riad");
+		list.add("Rifat");
+		list.add("Tofael");
+		list.add("Ahmed");
+		list.add("Hasan");
+
+		//rechecking the array list.
+		System.out.println("Size of Array list is: " + list.size());
+
+		//displaying all the elements of array list
+		System.out.println("List of all the elements: " +list);
+
+		//removing element and also by index number
+		list.remove("Hasan");
+		list.remove(0);
+		System.out.println("List after Removing" +list);
+
+		//checking if the list contain a searching element like "Riad"
+		System.out.println(list.contains("Riad"));
 	}
 
 }
